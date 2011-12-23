4ed3992f8b70f333caad055b46baa58f88d63e0b
Steve Yen <steve.yen@gmail.com>
2011-12-22 17:09:53 -0800

    Single quotes.

--- replicake.js
+++ replicake.js
@@ -1,7 +1,7 @@
 var assert = require('assert');
 
 exports.run_replica = function(conf) {
-  return exports.init_replica(conf).go("warming");
+  return exports.init_replica(conf).go('warming');
 }
 
 exports.init_replica = function(conf) {
@@ -10,26 +10,26 @@ exports.init_replica = function(conf) {
   var seeds     = conf.get('seeds');
   var name      = conf.get('name');
 
-  console.info("replicake...")
-  console.info("  time_start: " + time_start.toJSON());
-  console.info("  data_dir: " + data_dir);
-  console.info("  seeds: " + seeds);
-  console.info("  name: " + name);
+  console.info('replicake...')
+  console.info('  time_start: ' + time_start.toJSON());
+  console.info('  data_dir: ' + data_dir);
+  console.info('  seeds: ' + seeds);
+  console.info('  name: ' + name);
 
-  var state = { curr: "initial" };
+  var state = { curr: 'initial' };
   var log_db = null;
 
-  from_to(state, "initial", "warming",
+  from_to(state, 'initial', 'warming',
           function() {
             log_db_load(data_dir, name, conf.get('log_db'),
                         function(err, log_db_in) {
                           assert(log_db == null, 'log_db should be null')
                           log_db = log_db_in;
-                          go(state, "running");
+                          go(state, 'running');
                         });
           });
 
-  from_to(state, "warming", "running",
+  from_to(state, 'warming', 'running',
           function() {
             assert(log_db);
             // Concurrently...
@@ -42,18 +42,18 @@ exports.init_replica = function(conf) {
             // -- handle configuration changes
           });
 
-  from_to(state, "warming", "cooling", cool);
-  from_to(state, "running", "cooling", cool);
-  from_to(state, "cooling", "stopped", function() {});
+  from_to(state, 'warming', 'cooling', cool);
+  from_to(state, 'running', 'cooling', cool);
+  from_to(state, 'cooling', 'stopped', function() {});
 
   function cool() {
     log_db_save(log_db, function() {
-                  go(state, "stopped");
+                  go(state, 'stopped');
                 });
   }
 
   var self = {
-    "go": function(to_state) { go(state, to_state); return self; }
+    'go': function(to_state) { go(state, to_state); return self; }
   };
 
   return self;
@@ -71,17 +71,17 @@ function log_db_save(log_db, cb) {
 
 function from_to(state, from_state, to_state, cb) {
   state.transitions = state.transitions || {};
-  assert(state.transitions[from_state + " => " + to_state] == null);
-  state.transitions[from_state + " => " + to_state] = cb;
+  assert(state.transitions[from_state + ' => ' + to_state] == null);
+  state.transitions[from_state + ' => ' + to_state] = cb;
 }
 
 function go(state, to_state) {
   state.hist = state.hist || [];
   assert(state.curr);
-  var transition = state.curr + " => " + to_state;
-  console.info("transition: " + transition);
+  var transition = state.curr + ' => ' + to_state;
+  console.info('transition: ' + transition);
   assert(state.transitions[transition]);
-  state.hist[state.hist.length] = (new Date().toJSON()) + ": " + transition;
+  state.hist[state.hist.length] = (new Date().toJSON()) + ': ' + transition;
   state.curr = to_state;
   state.transitions[transition]();
 }
