c103822c1b3bde1709428bf534551f88a1f96591
Steve Yen <steve.yen@gmail.com>
2011-12-26 22:44:14 -0800

    Adding more WIP transitions.

--- replicake.js
+++ replicake.js
@@ -19,13 +19,9 @@ exports.open_replica = function(conf, log_db_module, routes) {
     return null;
   }
 
-  var state = { curr: 'opened' };
+  var state = { curr: 'opening' };
 
-  var paxos_proposer = null;
-  var paxos_acceptor = null;
-  var paxos_learner = null;
-
-  on_transition(state, 'opened', 'warming',
+  on_transition(state, 'opening', 'warming',
                 function() {
                   log_db_module.log_db_open(data_dir, name, conf.get('log_db'),
                                             function(err, log_db_in) {
@@ -39,6 +35,8 @@ exports.open_replica = function(conf, log_db_module, routes) {
   on_transition(state, 'warming', 'running',
                 function() {
                   assert(log_db);
+
+                  broadcast();
 
                   // Concurrently...
                   // -- find and/or help choose leader
@@ -64,6 +62,51 @@ exports.open_replica = function(conf, log_db_module, routes) {
       });
   }
 
+  var paxos_proposer = null;
+  var paxos_acceptor = null;
+  var paxos_learner = null;
+
+  routes_register('/paxos/promise_req');
+  routes_register('/paxos/promise_res');
+  routes_register('/paxos/accept_req');
+  routes_register('/paxos/accept_res');
+
+  function routes_register(message) {
+    routes.post('/' + message,
+                function(req, res) { go(state, message, req, res); });
+  }
+
+  on_transition(state, 'running', 'leader_ping_req', leader_ping_req, 'running');
+  on_transition(state, 'running', 'leader_ping_res', leader_ping_res, 'running');
+  on_transition(state, 'running', 'leader_ping_timeout', leader_ping_timout, 'running');
+  on_transition(state, 'running', 'catchup_req', catchup_req, 'running');
+  on_transition(state, 'running', 'catchup_res', catchup_res, 'running');
+  on_transition(state, 'running', 'catchup_timeout', catchup_timeout, 'running');
+
+  on_transition(state, 'running', 'leader_race_run', todo, 'running');
+  on_transition(state, 'running', 'leader_race_win', todo, 'running');
+  on_transition(state, 'running', 'leader_race_lose', todo, 'running');
+  on_transition(state, 'running', 'leader_race_timeout', todo, 'running');
+
+  on_transition(state, 'running', 'log_entry_learned', todo, 'running');
+  on_transition(state, 'running', 'log_gc_timeout', todo, 'running');
+
+  on_transition(state, 'running', 'new_config', todo, 'running');
+
+  on_transition(state, 'running', 'snapshot_timeout', todo, 'running');
+
+  function leader_ping_req() {};
+  function leader_ping_res() {};
+  function leader_ping_timeout() {};
+
+  function catchup_req() {};
+  function catchup_res() {};
+  function catchup_timeout() {};
+
+  function broadcast() {};
+
+  function todo() {};
+
   var self = {
     'go': function(to_state) { go(state, to_state); return self; },
     'warm': function() { self.go("warming"); return self; }
@@ -74,19 +117,19 @@ exports.open_replica = function(conf, log_db_module, routes) {
 
 // State machine helpers.
 
-function on_transition(state, from_state, to_state, cb) {
+function on_transition(state, from_state, to_state, cb, to_state_actual) {
   state.transitions = state.transitions || {};
   assert(state.transitions[from_state + ' => ' + to_state] == null);
-  state.transitions[from_state + ' => ' + to_state] = cb;
+  state.transitions[from_state + ' => ' + to_state] = [cb, to_state_actual];
 }
 
-function go(state, to_state) {
+function go(state, to_state, arg0, arg1) {
   state.hist = state.hist || [];
   assert(state.curr);
   var transition = state.curr + ' => ' + to_state;
   console.info('transition: ' + transition);
   assert(state.transitions[transition]);
   state.hist[state.hist.length] = (new Date().toJSON()) + ': ' + transition;
-  state.curr = to_state;
-  state.transitions[transition]();
+  state.curr = state.transitions[transition][1] || to_state;
+  state.transitions[transition][0](arg0, arg1);
 }
